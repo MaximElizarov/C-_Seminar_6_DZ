bb2028af48224c51a82b04144aa2b8440afd036f
Maxim Elizmarov <kiro.dota322@gmail.com>
2022-12-11 14:50:42 +0300

    add DZ

---
+++ Dop_treygolnik/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+Console.Write("Введите x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x3: ");
+double x3 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y3: ");
+double y3 = Convert.ToDouble(Console.ReadLine()); 
+double s = 0;
+
+s = -0.5 * ((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1));
+Console.WriteLine($"Площадь треугольника равна: {s}");
+
+// По целочисленным координатам вершин треугольника (x1,y1), (x2,y2) и (x3,y3) требуется вычислить его площадь.
+
+// Входные данные
+// Входной файл INPUT.TXT содержит 6 целых чисел x1,y1,x2,y2,x3,y3 – координаты вершин треугольника. Все числа не превышают 106 по абсолютной величине.
+
+// Выходные данные
+// В выходной файл OUTPUT.TXT выведите точное значение площади заданного треугольника.
+
+// Примеры
+// №	INPUT.TXT	OUTPUT.TXT
+// 1	2 1 2 4 6 1	6
+// 2	0 0 0 3 3 0	4.5
---
+++ Zadahya_41/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    array[i] = x;
+    if(array[i] > 0)
+    {
+       count++; 
+    }
+}
+Console.WriteLine($"Кол-во чисел больше 0: {count}");
+
+
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
